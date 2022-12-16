a7a5edac531fad9e6392524e76b5c2d829c2b096
Suhyeon jin <shjin1019@naver.com>
2022-12-16 16:20:23 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className='gray-background'>
-      <img src={logo} lat='logo' />
-      <h2>Let's develope management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "진수현",
+    birthday: "010602",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "홍길동",
+    birthday: "011102",
+    gender: "남자",
+    job: "백수",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "장태녕",
+    birthday: "011102",
+    gender: "남자",
+    job: "빵꾸",
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
